6ceefd35c020e1b57d16c982267ba2f24288d080
Charlie <charlie@wizeline.com>
2018-12-02 13:05:10 -0600

    solve day 02 of aoc

--- adventofcode2018/day02/day02.ts
+++ adventofcode2018/day02/day02.ts
@@ -1,22 +1,43 @@
-import { input } from "../day01/day01.input";
+import { input } from "./day02.input";
 
-const getFinalFrequency = (frequencies: number[]) => {
-  const set = new Set();
-  let counter = 0;
-  let index = 0;
-  while (true) {
-    if (index === frequencies.length) {
-      index = 0;
-    }
-
-    counter += frequencies[index];
-    if (set.has(counter)) {
-      return counter;
-    }
-
-    set.add(counter);
-    index++;
+const getCounters = (word: string) => {
+  const hash = {};
+  for (let j = 0; j < word.length; j++) {
+    const letter = word[j];
+    hash[letter] = hash[letter] || 0;
+    hash[letter]++;
   }
+
+  const keys = Object.keys(hash);
+  const result = { two: 0, three: 0 };
+  for (let j = 0; j < keys.length; j++) {
+    const key = keys[j];
+    if (hash[key] === 2) {
+      result.two = 1;
+    }
+
+    if (hash[key] === 3) {
+      result.three = 1;
+    }
+
+    if (result.two === 1 && result.three === 1) {
+      break;
+    }
+  }
+
+  return result;
 };
 
-console.log(getFinalFrequency(input));
+const getCheckSum = (boxIds: string[]) => {
+  let twoFactor = 0;
+  let threeFactor = 0;
+  for (let i = 0; i < boxIds.length; i++) {
+    const { two, three } = getCounters(boxIds[i]);
+    twoFactor += two;
+    threeFactor += three;
+  }
+
+  return twoFactor * threeFactor;
+};
+
+console.log(getCheckSum(input));
